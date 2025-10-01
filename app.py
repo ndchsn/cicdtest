49b1ad7c05aba03ca7a3be7617440a28bc830a2a
ndchsn <andiichsan123@gmail.com>
2025-10-01 12:45:28 +0000

    push app.py baru 3

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 @app.get("/") 
-    def read_root(): 
+def read_root(): 
     return {"message": "Hello from Cloud Run via CI/CD!"} 
 
 
 @app.get("/health") 
-    def health(): 
+def health(): 
     return {"status": "ok"}
