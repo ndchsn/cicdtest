083d1c23d547fb4ec7ed6b62e444bb99b8f7ecee
ndchsn <andiichsan123@gmail.com>
2025-10-01 12:42:22 +0000

    push app.py baru

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from app.main import app 
+# noqa: F401 
+
+app = FastAPI() 
 
 
-app = FastAPI()
+@app.get("/") 
+def read_root(): 
+return {"message": "Hello from Cloud Run via CI/CD!"} 
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Cloud Run via CI/CD!"}
-
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
+@app.get("/health") 
+def health(): 
+return {"status": "ok"}
